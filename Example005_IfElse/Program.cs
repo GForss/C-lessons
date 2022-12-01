69c0372462654c35f8a3f08f1c84e894d0363b77
GForss <konst665@gmail.com>
2022-12-01 17:44:59 +0300

    first

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "константин") 
+{
+    Console.Write("Привет, милый!");
+}
+else 
+{
+   Console.Write("Привет"); 
+}
