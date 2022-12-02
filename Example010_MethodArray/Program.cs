eff3fabd66f91ea579091987aa531214d761b072
GForss <konst665@gmail.com>
2022-12-02 16:37:12 +0300

    День 2

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+int[] array = {4, 8, 87, 54, 89, 88, 24, 89, 95};
+int n = array.Length;
+int find = 89;
+int index = 0;
+
+while (index < n) {
+    if (array[index] == find) {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
---
+++ Example011_ArrayLibray/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+vold FillArray(int[] collection) {
+    int length = collection.Length;
+    int index = 0;
+    while (index < length) {
+        collection[index] = new Random().Next(1, 12);
+        index++;
+    }
+}
+
+int[] array = new int[10];
